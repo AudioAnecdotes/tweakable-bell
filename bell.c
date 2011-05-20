1b69e4f7305d53bdc9dcac26f8182c3bb44d54ef
Robert Quattlebaum <darco@deepdarc.com>
2011-05-20 14:15:00 -0700

    Re-factored bell.c, added bell.h.

--- bell.c
+++ bell.c
@@ -1,522 +1,407 @@
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 #include <string.h>
 
-#include <pablio.h>
+#include "bell.h"
 
-#include <getopt.h>
+struct aa_bell_s {
+	int		bufferSize;
 
-#include <termios.h>
-#include <sys/ioctl.h>
-#include <sys/wait.h>
+	/** Mode frequencies in Hertz. */
+	float * f;
 
-/** Modal model, which is loaded from an .sy format text file.
-        @author Kees van den Doel (kvdoel@cs.ubc.ca)
-*/
+	/**  Angular decay rates in Hertz. */
+	float * d;
 
-/** Mode frequencies in Hertz. */
-float *f;
+	/** Gains. a[p][k] is gain at point p for mode k. */
+	float * a;
 
-/**  Angular decay rates in Hertz. */
-float *d;
+	/** Number of modes available. */
+	int		nf;
 
-/** Gains. a[p][k] is gain at point p for mode k. */
-float *a;
+	/** Number of modes used. */
+	int		nfUsed;
 
-/** Number of modes available. */
-int nf;
+	/** Number of points. */
+	int		np;
 
-/** Number of modes used. */
-int nfUsed;
+	/** Multiplies all frequencies. */
+	float	fscale;
 
-/** Number of points. */
-int np;
+	/** Multiplies all dampings. */
+	float	dscale;
 
-/** Multiplies all frequencies. */
-float fscale;
+	/** Multiplies all gains. */
+	float	ascale;
 
-/** Multiplies all dampings. */
-float dscale;
 
-/** Multiplies all gains. */
-float ascale;
+	float * cosForce;
 
-/** Allocated arrays.
-    @param nf number of modes.
-    @param np number of locations.
-*/
 
-// globals
-FILE *gnuplot = NULL;
-static struct termios Otty, Ntty;
+	/** Sampling rate in Hertz. */
+	float	srate;
 
-void
-allocateArrays(int nf, int np)
-{
-	f = (float *)malloc(sizeof(float) * nf);
-	d = (float *)malloc(sizeof(float) * nf);
-	a = (float *)malloc(sizeof(float) * nf * np);
-	fscale = dscale = ascale = 1.f;
+	/** State of filters. */
+	float * yt_1, *yt_2;
+
+	/** The transfer function of a reson filter is H(z) = 1/(1-twoRCosTheta/z + R2/z*z). */
+	float * R2;
+
+	/** The transfer function of a reson filter is H(z) = 1/(1-twoRCosTheta/z + R2/z*z). */
+	float * twoRCosTheta;
+
+	/** Cached values. */
+	float * c_i;
+
+	/** Reson filter gain vector. */
+	float * ampR;
 };
 
 void
-readModes(FILE * fp)
-{
+aa_bell_dump(
+	aa_bell_t self, FILE* outfile
+) {
+	fprintf(outfile, "nactive_freq: %d\n", self->nfUsed);
+	fprintf(outfile, "n_freq: %d\n", self->nf);
+	fprintf(outfile, "n_points: %d\n", self->np);
+	fprintf(outfile, "f_scale: %f\n", self->fscale);
+	fprintf(outfile, "d_scale: %f\n", self->dscale);
+	fprintf(outfile, "a_scale: %f\n", self->ascale);
+	fprintf(outfile, "frequencies and dampings:\n");
+	for(int i = 0; i < self->nf; i++) {
+		fprintf(outfile, "\t%d: \t%f \t%f\n", i, self->f[i], self->d[i]);
+	}
+	fprintf(outfile, "amplitudes[point][freq]:\n");
+	for(int i = 0; i < self->nf; i++) {
+		fprintf(outfile, "\t%d:", i);
+		for(int p = 0; p < self->np; p++) {
+			fprintf(outfile, " \t%f", self->a[p * self->nf + i]);
+		}
+		fprintf(outfile, "\n");
+	}
+
+	fprintf(outfile, "R2 yt_1 yt_2 twoRCosTheta c_i ampR:\n");
+	for(int i = 0; i < self->nf; i++) {
+		fprintf(outfile,
+			"\t%d: %f %f %f %f %f %f\n",
+			i,
+			self->R2[i],
+			self->yt_1[i],
+			self->yt_2[i],
+			self->twoRCosTheta[i],
+			self->c_i[i],
+			self->ampR[i]);
+	}
+
+	fprintf(outfile, "Force:\n");
+	for(int i = 0; i < self->bufferSize; i++) {
+		fprintf(outfile, "\t%d: %f\n", i, self->cosForce[i]);
+	}
+	fprintf(outfile, "-------\n");
+}
+
+aa_bell_t
+aa_bell_create(
+	int nf, int np, int bufferSize, int srate
+) {
+	aa_bell_t ret = NULL;
+
+	ret = calloc(sizeof(*ret), 1);
+
+	if(!ret)
+		goto bail;
+
+
+	ret->fscale = ret->dscale = ret->ascale = 1.0f;
+	ret->nfUsed = ret->nf = nf;
+	ret->np = np;
+	if(srate)
+		ret->srate = srate;
+	else
+		ret->srate = AA_BELL_DEFAULT_SRATE;
+	ret->bufferSize = bufferSize;
+
+	ret->f = (float*)malloc(sizeof(float) * nf);
+
+	if(!ret->f) {
+		aa_bell_release(ret);
+		ret = NULL;
+		goto bail;
+	}
+
+	ret->d = (float*)calloc(sizeof(float), nf);
+
+	if(!ret->d) {
+		aa_bell_release(ret);
+		ret = NULL;
+		goto bail;
+	}
+
+	ret->a = (float*)calloc(sizeof(float), nf * np);
+
+	if(!ret->a) {
+		aa_bell_release(ret);
+		ret = NULL;
+		goto bail;
+	}
+
+	ret->cosForce = (float*)calloc(sizeof(float), bufferSize);
+
+	ret->R2 = (float*)calloc(sizeof(float), nf);
+	ret->twoRCosTheta = (float*)calloc(sizeof(float), nf);
+	ret->yt_1 = (float*)calloc(sizeof(float), nf);
+	ret->yt_2 = (float*)calloc(sizeof(float), nf);
+	ret->c_i = (float*)calloc(sizeof(float), nf);
+	ret->ampR = (float*)calloc(sizeof(float), nf);
+
+
+bail:
+	return ret;
+}
+
+void
+aa_bell_release(aa_bell_t self) {
+	free(self->f);
+	free(self->d);
+	free(self->a);
+	free(self->R2);
+	free(self->twoRCosTheta);
+	free(self->yt_1);
+	free(self->yt_2);
+	free(self->c_i);
+	free(self->ampR);
+	free(self->cosForce);
+	free(self);
+}
+
+aa_bell_t
+aa_bell_create_from_file(
+	const char *path, int bufferSize, int srate
+) {
+	aa_bell_t ret = NULL;
 	float dval;
 	int ival;
-	char *s;
+	int nfUsed, nf, np;
+	char s[256];
+	FILE* fp;
 
-	s = (char *)malloc(256);
+	fp = fopen(path, "r");
+
+	if(!fp)
+		goto bail;
 
 	fscanf(fp, "%s\n", s);
-	// s is now: "nactive_freq:"    
+	// s is now: "nactive_freq:"
 	fscanf(fp, "%d\n", &nfUsed);
 	//printf(":%s:\n",s);
-	fscanf(fp, "%s\n", s);		// n_freq:
+	fscanf(fp, "%s\n", s);      // n_freq:
 	fscanf(fp, "%d\n", &nf);
-	fscanf(fp, "%s\n", s);		// n_points:
+	fscanf(fp, "%s\n", s);      // n_points:
 	fscanf(fp, "%d\n", &np);
-	fscanf(fp, "%s\n", s);		// f_scale:
-	fscanf(fp, "%f\n", &fscale);
-	fscanf(fp, "%s\n", s);		// d_scale:
-	fscanf(fp, "%f\n", &dscale);
-	fscanf(fp, "%s\n", s);		// a_scale:
-	fscanf(fp, "%f\n", &ascale);
 
-	allocateArrays(nf, np);
+	ret = aa_bell_create(nf, np, bufferSize, srate);
 
-	fscanf(fp, "%s\n", s);		// frequencies:
-	for (int i = 0; i < nf; i++) {
-		fscanf(fp, "%f\n", &f[i]);
+	if(!ret)
+		goto bail;
+
+	fscanf(fp, "%s\n", s);      // f_scale:
+	fscanf(fp, "%f\n", &ret->fscale);
+	fscanf(fp, "%s\n", s);      // d_scale:
+	fscanf(fp, "%f\n", &ret->dscale);
+	fscanf(fp, "%s\n", s);      // a_scale:
+	fscanf(fp, "%f\n", &ret->ascale);
+
+	fscanf(fp, "%s\n", s);      // frequencies:
+	for(int i = 0; i < nf; i++) {
+		fscanf(fp, "%f\n", &ret->f[i]);
 	}
-	fscanf(fp, "%s\n", s);		// dampings:
-	for (int i = 0; i < nf; i++) {
-		fscanf(fp, "%f\n", &d[i]);
+	fscanf(fp, "%s\n", s);      // dampings:
+	for(int i = 0; i < nf; i++) {
+		fscanf(fp, "%f\n", &ret->d[i]);
 	}
-	fscanf(fp, "%s\n", s);		// amplitudes[point][freq]:
-	for (int p = 0; p < np; p++) {
-		for (int i = 0; i < nf; i++) {
-			fscanf(fp, "%f\n", &a[p * nf + i]);
+	fscanf(fp, "%s\n", s);      // amplitudes[point][freq]:
+	for(int p = 0; p < np; p++) {
+		for(int i = 0; i < nf; i++) {
+			fscanf(fp, "%f\n", &ret->a[p * nf + i]);
 		}
 	}
-	fscanf(fp, "%s\n", s);		// END
-};
 
-/** Read the modes file in .sy format.
-    @param fn File name with modal data in .sy format.
-*/
-void
-readModesFile(char *fn)
-{
-	FILE *fp = fopen(fn, "r");
-	readModes(fp);
-	fclose(fp);
-};
+	aa_bell_compute_filter(ret);
 
-/** Vibration model of object, capable of playing sound.
-    @author Kees van den Doel (kvdoel@cs.ubc.ca)
-*/
+bail:
+	if(fp)
+		fclose(fp);
 
-/** Sampling rate in Hertz. */
-float srate = 44100.f;
+	return ret;
+}
 
-/** State of filters. */
-float *yt_1, *yt_2;
-
-/** The transfer function of a reson filter is H(z) = 1/(1-twoRCosTheta/z + R2/z*z). */
-float *R2;
-
-/** The transfer function of a reson filter is H(z) = 1/(1-twoRCosTheta/z + R2/z*z). */
-float *twoRCosTheta;
-
-/** Cached values. */
-float *c_i;
-
-/** Reson filter gain vector. */
-float *ampR;
 
 /** Compute gains of contact
-*/
+ */
 void
-computeLocation()
-{
-	for (int i = 0; i < nf; i++) {
-		ampR[i] = ascale * c_i[i] * a[i];
-	}
+aa_bell_compute_location(
+	aa_bell_t self, int i
+) {
+	self->ampR[i] = self->ascale * self->c_i[i] * self->a[i];
 }
 
 /** Set state to non-vibrating.
-*/
+ */
 void
-clearHistory()
-{
-	for (int i = 0; i < nf; i++) {
-		yt_1[i] = yt_2[i] = 0;
+aa_bell_clear_history(aa_bell_t self) {
+	for(int i = 0; i < self->nf; i++) {
+		self->yt_1[i] = self->yt_2[i] = 0;
 	}
 }
 
 void
-dampResonators(double damping)
-{
-	for (int i = 0; i < nf; i++) {
-		yt_1[i] *= damping;
-		yt_2[i] *= damping;
+aa_bell_damp_resonators(
+	aa_bell_t self, double damping
+) {
+	for(int i = 0; i < self->nf; i++) {
+		self->yt_1[i] *= damping;
+		self->yt_2[i] *= damping;
 	}
-}
-
-/** Allocate data.
-    @param nf number of modes.
-    @param np number of locations.
-*/
-void
-allocateData(int nf, int np)
-{
-	R2 = (float *)malloc(sizeof(float) * nf);
-	twoRCosTheta = (float *)malloc(sizeof(float) * nf);
-	yt_1 = (float *)malloc(sizeof(float) * nf);
-	yt_2 = (float *)malloc(sizeof(float) * nf);
-	c_i = (float *)malloc(sizeof(float) * nf);
-	ampR = (float *)malloc(sizeof(float) * nf);
-	clearHistory();
 }
 
 /** Compute the reson coefficients from the modal model parameters.
     Cache values for location computation.
-*/
+ */
 void
-computeResonCoeff()
-{
-	for (int i = 0; i < nf; i++) {
-		float tmp_r = (float)(exp(-dscale * d[i] / srate));
-		R2[i] = tmp_r * tmp_r;
-		twoRCosTheta[i] =
-			(float)(2. * cos(2. * M_PI * fscale * f[i] / srate) * tmp_r);
-		c_i[i] = (float)(sin(2. * M_PI * fscale * f[i] / srate) * tmp_r);
-	}
+aa_bell_compute_reson_coeff(
+	aa_bell_t self, int i
+) {
+	float tmp_r = (float)(exp(-self->dscale * self->d[i] / self->srate));
+
+	self->R2[i] = tmp_r * tmp_r;
+	self->twoRCosTheta[i] =
+	    (float)(2. *
+	    cos(2. * M_PI * self->fscale * self->f[i] / self->srate) * tmp_r);
+	self->c_i[i] =
+	    (float)(sin(2. * M_PI * self->fscale * self->f[i] /
+			self->srate) * tmp_r);
 }
 
 /** Compute the filter coefficients used for real-time rendering
     from the modal model parameters.
-*/
+ */
 void
-computeFilter()
-{
-	computeResonCoeff();
-	computeLocation();
+aa_bell_compute_filter(aa_bell_t self) {
+	for(int i = 0; i < self->nf; i++) {
+		aa_bell_compute_reson_coeff(self, i);
+		aa_bell_compute_location(self, i);
+	}
 }
 
-/** Compute response through bank of modal filters.
-    @param output provided output buffer.
-*/
 double
-computeSoundBuffer(float *output,
-	float *force, int nsamples, bool doPlot, int numResonators)
-{
-	static unsigned plotCount = 0;
+aa_bell_compute_sound_buffer(
+	aa_bell_t	self,
+	float *		output
+) {
 	double total = 0.0;
+	int numResonators = self->nfUsed;
+	int nsamples = self->bufferSize;
 
-	for (int k = 0; k < nsamples; k++) {
-		output[k] = 0;
-	}
-	int nf = nfUsed;
+	memset((void*)output, 0, sizeof(float) * nsamples);
 
-	if (doPlot) {
-		//if((plotCount == 0) && (i == 0)) { // XXX this one is if we moved this inside the loop to only plot for f0
-		if (plotCount == 0) {
-			fprintf(gnuplot, "set style data lines\n");
-			//fprintf(gnuplot, "set xrange[0:1000]\n");
-			fprintf(gnuplot, "set yrange[-1:1]\n");
-			fprintf(gnuplot, "unset key\n");
+	for(int i = 0; i < numResonators; i++) {
+		float tmp_twoRCosTheta = self->twoRCosTheta[i];
+		float tmp_R2 = self->R2[i];
+		float tmp_a = self->ampR[i];
+		float tmp_yt_1 = self->yt_1[i];
+		float tmp_yt_2 = self->yt_2[i];
 
-			fprintf(gnuplot, "plot \"-\"\n");
-		}
-	}
-
-	for (int i = 0; i < numResonators; i++) {
-		float tmp_twoRCosTheta = twoRCosTheta[i];
-		float tmp_R2 = R2[i];
-		float tmp_a = ampR[i];
-		float tmp_yt_1 = yt_1[i];
-		float tmp_yt_2 = yt_2[i];
-		for (int k = 0; k < nsamples; k++) {
+		for(int k = 0; k < nsamples; k++) {
 			float ynew = tmp_twoRCosTheta * tmp_yt_1 - tmp_R2 * tmp_yt_2
-				+ tmp_a * force[k];
+			    + tmp_a * self->cosForce[k];
 			tmp_yt_2 = tmp_yt_1;
 			tmp_yt_1 = ynew;
 			output[k] += ynew;
 
-			if (i == 0)			// only total f0
+
+			if(i == 0)          // only total f0
 				total += fabs(ynew);
-
-#ifdef PLOTEACH
-			if ((doPlot) && (i == 0)) {
-				if (!(plotCount % 20))	// try plotting every 10th pt
-					fprintf(gnuplot, "%d %f\n", plotCount, ynew);
-
-				if (plotCount++ > 1000) {
-					fprintf(gnuplot, "e\n");
-					plotCount = 0;
-				}
-
-			}
-#endif							/* PLOTEACH */
 		}
-		yt_1[i] = tmp_yt_1;
-		yt_2[i] = tmp_yt_2;
+		self->yt_1[i] = tmp_yt_1;
+		self->yt_2[i] = tmp_yt_2;
 	}
 
-	if (gnuplot) {
-		for (int k = 0; k < nsamples; k += 10) {
-			fprintf(gnuplot, "%d %f\n", plotCount, output[k]);
+	memset((void*)self->cosForce, 0, sizeof(float) * self->bufferSize);
 
-			if (plotCount++ > 500) {
-				fprintf(gnuplot, "e\n");
-				plotCount = 0;
-			}
-		}
+	for(int i = 0; i < self->bufferSize; i++) {
+//		output[i] /= numResonators;
+		// printf("%f\n",buffer[i]);
+		// XXX Realy should saturate...
+		if(output[i] > 1.0)
+			output[i] = 1.0;
+		if(output[i] < -1.0)
+			output[i] = -1.0;
 	}
 
-	return (total);
+	return total;
 }
 
 void
-cleanup()
-{
-	exit(0);
+aa_bell_add_energy(
+	aa_bell_t self, float energy, float dur
+) {
+	int nsamples = (int)(self->srate * dur);
+
+	if(nsamples > self->bufferSize)
+		nsamples = self->bufferSize;
+	if(nsamples <= 1) {
+		self->cosForce[0] = energy;
+	} else {
+		for(int i = 0; i < nsamples; i++) {
+			self->cosForce[i] +=
+			    (float)(energy *
+			        (1. - cos(2 * M_PI * (i + 1) / (1 + nsamples))));
+		}
+	}
 }
+
 
 void
-received_interrupt(int unused)
-{
-	cleanup();
+aa_bell_set_mode_freq(
+	aa_bell_t self, int res_index, float val
+) {
+	if(res_index < self->nf)
+		self->f[res_index] = val;
 }
 
-void
-cleanup_tty()
-{
-	tcsetattr(0, TCSANOW, &Otty);
+void aa_bell_set_angular_decay(
+	aa_bell_t self, int res_index, float val
+) {
+	if(res_index < self->nf)
+		self->d[res_index] = val;
 }
 
-#define min(x, y) (x<y)?x:y
+void aa_bell_set_gain(
+	aa_bell_t self, int point, int mode, float val
+) {
+	if((mode < self->nf) && (point < self->np))
+		self->a[mode + point * self->nf] = val;
+}
 
-int
-main(int argc, char *argv[]
-	)
-{
-	// int bufferSize = 4096;
-	int bufferSize = 10;
+int aa_bell_get_mode_count(aa_bell_t self) {
+	return self->nf;
+}
 
-	bool doInput = true;
-	bool doImpulse = false;
-	bool doPlot = false;
-	bool doInteraction = true;
-	unsigned plotCount = 0;
+int aa_bell_get_point_count(aa_bell_t self) {
+	return self->np;
+}
 
-	int numResonators;
-	enum { resonators, tuneFreq, tuneDamp, tuneAmp } mode = resonators;
+int aa_bell_get_used_mode_count(aa_bell_t self) {
+	return self->nfUsed;
+}
 
-	int c;
-	while ((c = getopt(argc, argv, "ig")) != -1) {
-		switch (c) {
-		case 'i':
-			doImpulse = true;
-			doInput = false;	// mutually exclusive for now...
-			break;
+void aa_bell_set_used_mode_count(
+	aa_bell_t self, int nfUsed
+) {
+	self->nfUsed = nfUsed;
+}
 
-		case 'g':
-			doPlot = true;
-			break;
-
-		case '?':
-			fprintf(stderr, "%s: Unknown argument %c\n", argv[0], optopt);
-			cleanup();
-
-		default:
-			break;
-		}
-	}
-
-	printf("argc:%d optind:%d\n", argc, optind);
-	if (argc != (optind + 1)) {
-		fprintf(stderr, "%s: [-i -g] resonant filename\n", argv[0]);
-		cleanup();
-	}
-
-	printf("%s, %s\n", doPlot ? "PLOT" : "!plot", doInput ? "INPUT" : "!input");
-
-	if (doPlot) {
-		gnuplot = popen("gnuplot", "w");
-		setvbuf(gnuplot, NULL, _IONBF, 0);	// use unbuffered writes
-	}
-
-	PABLIO_Stream *inStream, *outStream;
-	if (doInput)
-		OpenAudioStream(&inStream, srate, paFloat32, PABLIO_READ | PABLIO_MONO);
-	OpenAudioStream(&outStream, srate, paFloat32, PABLIO_WRITE | PABLIO_MONO);
-
-	readModesFile(argv[optind]);
-	numResonators = nf;
-
-	if (doInteraction) {
-		int status;
-
-		tcgetattr(0, &Otty);
-		signal(SIGINT, &received_interrupt);
-		Ntty = Otty;
-
-		Ntty.c_lflag = ISIG|ECHONL;		// line settings (no echo)
-
-		Ntty.c_cc[VMIN] = 0;	// minimum time to wait
-		Ntty.c_cc[VTIME] = 0;	// minimum characters to wait for
-		status = tcsetattr(0, TCSANOW, &Ntty);
-		if (status)
-			fprintf(stderr, "tcsetattr returned %d\n", status);
-		atexit(&cleanup_tty);
-	}
-
-	if (doImpulse)
-		bufferSize = 4096;
-	else
-		bufferSize = 10;
-
-	float *buffer = (float *)malloc(bufferSize * sizeof(float));
-
-	printf("nactive_freq: %d\n", nfUsed);
-	printf("n_freq: %d\n", nf);
-	printf("n_points: %d\n", np);
-	printf("f_scale: %f\n", fscale);
-	printf("d_scale: %f\n", dscale);
-	printf("a_scale: %f\n", ascale);
-	printf("frequencies:\n");
-	for (int i = 0; i < nf; i++) {
-		printf("%f\n", f[i]);
-	}
-	printf("dampings:\n");
-	for (int i = 0; i < nf; i++) {
-		printf("%f\n", d[i]);
-	}
-	printf("amplitudes[point][freq]:\n");
-	for (int p = 0; p < np; p++) {
-		for (int i = 0; i < nf; i++) {
-			printf("%f\n", a[p * nf + i]);
-		}
-	}
-
-	allocateData(nf, np);
-	computeFilter();
-
-	float dur = .0002f;			// 2 ms
-	int nsamples = (int)(srate * dur);
-	printf("nsamples: %d\n", nsamples);
-	float *cosForce = (float *)malloc(sizeof(float) * bufferSize);
-	memset(cosForce, 0, bufferSize * sizeof(float));
-	for (int i = 0; i < nsamples; i++) {
-		cosForce[i] =
-			(float)(.5 * (1. - cos(2 * M_PI * (i + 1) / (1 + nsamples))));
-	}
-
-	int x;
-	bool done = false;
-	while (!done) {
-		char car;
-		double total;
-		int num = 0;
-
-		if (doInput)
-			ReadAudioStream(inStream, cosForce, bufferSize);
-
-		car = fgetc(stdin);
-		if (car != -1) {
-			switch (car) {
-			case ' ':
-				dampResonators(0.3);
-				break;
-
-			case '0':
-				numResonators = nf;
-				break;
-
-			case 'r':
-				mode = resonators;
-				break;
-			case 'f':
-				mode = tuneFreq;
-				printf("tuneFreq\n");
-				break;
-			case 'd':
-				mode = tuneDamp;
-				printf("tuneDamp\n");
-				break;
-			case 'a':
-				mode = tuneAmp;
-				printf("tuneAmp\n");
-				break;
-
-			case 'i':
-				for (int i = 0; i < nsamples; i++)
-					cosForce[i] =
-						(float)(.5 *
-						(1. - cos(2 * M_PI * (i + 1) / (1 + nsamples))));
-				break;
-
-			case 'q':
-				printf("calling cleanup\n");
-				cleanup();
-				break;
-			default:
-				if ((car == '+') || (car == '=') || (car == '-')) {
-					double delta = 0.1 * (car == '-') ? -1.0 : 1.0;
-
-					// update params
-					switch (mode) {
-					case tuneFreq:
-						f[num] += delta;
-						break;
-					case tuneDamp:
-						d[num] += delta;
-						break;
-					case tuneAmp:
-						a[num] += delta;
-						break;
-					}
-
-					computeFilter();	// rebuild filters (XXX eventually narrow it to rebuild what we need)
-				}
-
-				if ((car >= '1') && (car <= '9')) {
-					num = car - '0';
-
-					numResonators = min(nf, num);
-					printf("numResonators = %d\n", numResonators);
-				}
-			}
-		} else {
-			clearerr(stdin);
-		}
-		total =
-			computeSoundBuffer(buffer, cosForce, bufferSize, doPlot,
-			numResonators);
-		if (doImpulse && (total < 0.0001)) {
-			done = true;
-			printf("total = %f\n", total);
-		}
-		memset(cosForce, 0, nsamples * sizeof(float));
-		for (int i = 0; i < bufferSize; i++) {
-			buffer[i] /= nf;
-			// printf("%f\n",buffer[i]);
-			// XXX Realy should saturate...
-			if (buffer[i] > 1.0)
-				buffer[i] = 1.0;
-			if (buffer[i] < -1.0)
-				buffer[i] = -1.0;
-		}
-		WriteAudioStream(outStream, buffer, bufferSize);
-
-	}
-
-	CloseAudioStream(outStream);
-
-	return 0;
+float* aa_bell_get_cos_force_ptr(aa_bell_t self) {
+	return self->cosForce;
 }
---
+++ bell.h
@@ -0,0 +1,68 @@
+//
+//  bell.h
+//
+
+#ifndef __AA_BELL_H__
+#define __AA_BELL_H__ 1
+
+#if !defined(__BEGIN_DECLS) || !defined(__END_DECLS)
+#if defined(__cplusplus)
+#define __BEGIN_DECLS   extern "C" {
+#define __END_DECLS \
+	}
+#else
+#define __BEGIN_DECLS
+#define __END_DECLS
+#endif
+#endif
+
+#include <stddef.h>
+#include <stdint.h>
+
+__BEGIN_DECLS
+
+#define AA_BELL_DEFAULT_SRATE       (44100.0f)
+
+struct aa_bell_s;
+typedef struct aa_bell_s *aa_bell_t;
+
+aa_bell_t aa_bell_create(
+	int mode_count, int point_count, int bufferSize, int srate);
+
+aa_bell_t aa_bell_create_from_file(
+	const char *path, int bufferSize, int srate);
+void aa_bell_release(aa_bell_t x);
+
+void aa_bell_set_mode_freq(
+	aa_bell_t self, int res_index, float val);
+void aa_bell_set_angular_decay(
+	aa_bell_t self, int res_index, float val);
+void aa_bell_set_gain(
+	aa_bell_t self, int point, int mode, float val);
+int aa_bell_get_mode_count(aa_bell_t self);
+int aa_bell_get_point_count(aa_bell_t self);
+int aa_bell_get_used_mode_count(aa_bell_t self);
+void aa_bell_set_used_mode_count(
+	aa_bell_t self, int nfUsed);
+float* aa_bell_get_cos_force_ptr(aa_bell_t self);
+
+void aa_bell_compute_location(
+	aa_bell_t self, int i);
+void aa_bell_clear_history(aa_bell_t self);
+void aa_bell_damp_resonators(
+	aa_bell_t self, double damping);
+void aa_bell_compute_reson_coeff(
+	aa_bell_t self, int i);
+void aa_bell_add_energy(
+	aa_bell_t self, float energy, float dur);
+
+void aa_bell_compute_filter(aa_bell_t self);
+
+double aa_bell_compute_sound_buffer(
+	aa_bell_t self, float *output);
+
+void aa_bell_dump(
+	aa_bell_t self, FILE* outfile);
+
+__END_DECLS
+#endif                          // #ifndef __AA_BELL_H__
